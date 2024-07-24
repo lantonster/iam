152dff796153dfebffd88fdd68b2ba23fdae015a
lllllan <342310798@qq.com>
2024-07-24 14:53:07 +0800

    🧪 test(repo): 创建 GetUserByUsername 的测试

---
+++ test/repo/repo_test.go
@@ -0,0 +1,50 @@
+package repo
+
+import (
+	"testing"
+
+	"github.com/lantonster/corekit"
+	"github.com/lantonster/iam/config"
+	"github.com/lantonster/iam/internal/model"
+	"github.com/lantonster/iam/internal/repo"
+	"gorm.io/gorm"
+)
+
+var (
+	r  *repo.Repo
+	db *gorm.DB
+)
+
+func TestMain(m *testing.M) {
+	conf := config.NewConfig()
+	r = repo.NewRepo(conf)
+
+	// 清空数据库
+	cleardb(conf)
+
+	// 填充测试数据
+	testdata()
+
+	m.Run()
+}
+
+func cleardb(conf *config.Config) {
+	db = corekit.ConnectMySQL(conf.MySQL)
+
+	// 获取所有表名
+	tables := []string{}
+	db.Raw("SHOW TABLES").Scan(&tables)
+
+	// 循环使用 TRUNCATE TABLE 重置自增 ID 并清除数据
+	for _, table := range tables {
+		db.Exec("TRUNCATE TABLE " + table)
+	}
+}
+
+var (
+	testUser = model.User{Username: "test_user"}
+)
+
+func testdata() {
+	db.Create(&testUser)
+}
---
+++ test/repo/user_test.go
@@ -0,0 +1,26 @@
+package repo
+
+import (
+	"context"
+	"testing"
+
+	"github.com/lantonster/cerrors"
+	"github.com/lantonster/ecodes"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUserByUsername(t *testing.T) {
+	repo := r.User
+	ctx := context.Background()
+
+	// 测试存在用户的情况
+	user, err := repo.GetUserByUsername(ctx, testUser.Username)
+	assert.NoError(t, err)
+	assert.NotNil(t, user)
+
+	// 测试不存在用户的情况
+	user, err = repo.GetUserByUsername(ctx, "non_existent_user")
+	assert.Error(t, err)
+	assert.Nil(t, user)
+	assert.Equal(t, ecodes.IAM_USERNAME_NOT_FOUND, cerrors.Code(err))
+}
